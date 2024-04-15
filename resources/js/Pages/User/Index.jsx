047d6907b022063c6cc24eeeedc83750467449d9
VLuca997 <vluca997@gmail.com>
2024-04-15 12:41:56 +0200

    Copy Project folder, files still to be renamed

---
+++ resources/js/Pages/User/Index.jsx
@@ -0,0 +1,233 @@
+//LAYOUT USER AUTENTICATO.
+import Pagination from "@/Components/Pagination.jsx";
+import SelectInput from "@/Components/SelectInput.jsx";
+import TextInput from "@/Components/TextInput";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx"; // mappatura
+import { Head, Link, router } from "@inertiajs/react";
+
+// import {ChevronUpIcon, ChevronDownIcon} from "@heroicons/react/20/solid"; // pacchetto icone ora inserito nel componente TableHeading
+import TableHeading from "@/Components/TableHeading"; // componente per riutilizzare heading della tabella.
+
+export default function Index({auth, projects, queryParams = null, success}){
+
+
+    queryParams = queryParams || {};
+
+//SEARCHBAR FUNCTION X LA RICERCA TRAMITE NOME-----
+const searchFieldChanged =(name, value) => {
+    if(queryParams){
+        queryParams[name] = value;
+
+    }else{
+        delete queryParams[name];
+    }
+    //REDIRECT CON I PARAMETRI PASSATI
+    router.get(route('project.index'),queryParams);
+}
+
+//TASTO ENTER COME INPUT D'AVVIO
+const onKeyPress = (name, e) => {
+
+    if(e.key !== 'Enter') return;
+
+    searchFieldChanged(name, e.target.value);
+}
+//-------------------------------------------------
+//SMISTAMENTO:
+const sortChanged = (name) => {
+    if(name === queryParams.sort_field){
+        if(queryParams.sort_direction === 'asc'){
+            queryParams.sort_direction = 'desc';
+        }else{
+            queryParams.sort_direction = 'asc';
+        }
+    }else{
+        queryParams.sort_field = name;
+        queryParams.sort_direction = 'asc';
+    }
+    router.get(route("project.index"), queryParams );
+}
+
+//---------------------------------------------------------
+//DELETE FUNCION PROJECT-----------------------------------
+    const deleteProject = (project) => {
+        if(!window.confirm('Are you sure you want to delete the project?')){
+            return;
+        }
+        router.delete(route('project.destroy', project.id))
+    }
+//---------------------------------------------------------
+
+
+    return(
+        <AuthenticatedLayout
+            user={auth.user}
+            header=
+            {<div className="flex justify-between items-center">
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                        PROJECTS
+                    </h2>
+                    {/* ADD NEW PROJECT! */}
+                    <Link
+                        href= {route('project.create')}
+                        className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+                    >
+                        Add New Project!
+                    </Link>
+            </div>}
+        >
+        <Head title="Project" />
+
+        <div className="py-12">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                {success && (<div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+                    {success}
+                </div>)}
+
+                <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="p-6 text-gray-900 dark:text-gray-100">
+                        <div className="overflow-auto">
+                            <table className="w-full text-sm text-left trl:text-right text-gray-500 dark:text-gray-400">
+                                    {/* FILTRO CLICK SUL NOME  */}
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <TableHeading
+                                                name='id'
+                                                sort_field={queryParams.sort_field}
+                                                sort_direction = {queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                ID
+                                            </TableHeading>
+                                            <th className="px-3 py-2">Image</th>
+                                            <TableHeading
+                                                name='name'
+                                                sort_field={queryParams.sort_field}
+                                                sort_direction = {queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                NAME
+                                            </TableHeading>
+                                            <TableHeading
+                                                name='status'
+                                                sort_field={queryParams.sort_field}
+                                                sort_direction = {queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                STATUS
+                                            </TableHeading>
+                                            <TableHeading
+                                                name='created_at'
+                                                sort_field={queryParams.sort_field}
+                                                sort_direction = {queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                CREATE DATA
+                                            </TableHeading>
+                                            <TableHeading
+                                                name='due_date'
+                                                sort_field={queryParams.sort_field}
+                                                sort_direction = {queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                DUE DATE
+                                            </TableHeading>
+                                            <th className="px-3 py-2">Created By</th>
+                                            <th className="px-3 py-2 text-right">Actions</th>
+                                        </tr>
+                                    </thead>
+
+                                    {/* FILTRI A RICERCA  */}
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <th className="px-3 py-2"></th>
+                                            <th className="px-3 py-2"></th>
+                                            <th className="px-3 py-2">
+                                                {/* SEARCHBAR */}
+                                                <TextInput
+                                                    className="w-full"
+                                                    defaultValue = {queryParams.name}
+                                                    placeholder="Project name.."
+                                                    onBlur={e => searchFieldChanged('name', e.target.value)}
+                                                    onKeyPress={e => onKeyPress('name', e)}
+
+                                                />
+
+                                            </th>
+                                            <th className="px-3 py-2">
+                                                {/* SEARCH INPUT */}
+                                                <SelectInput
+                                                    className="w-full"
+                                                    defaultValue = {queryParams.status}
+                                                    onChange={e => searchFieldChanged('status' ,e.target.value)}
+                                                >
+                                                    <option value="">Select Status</option>
+                                                    <option value="pending">Pending</option>
+                                                    <option value="in_progress">In Progress</option>
+                                                    <option value="completed">Complete</option>
+
+                                                </SelectInput>
+                                                </th>
+                                            <th className="px-3 py-2"> </th>
+                                            <th className="px-3 py-2"> </th>
+                                            <th className="px-3 py-2"> </th>
+                                            <th className="px-3 py-2"></th>
+                                        </tr>
+                                    </thead>
+
+                                    {/* BODY SPA */}
+                                    <tbody>
+                                        {projects.data.map((project) => (
+                                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
+
+                                                <td className="px-3 py-2">{project.id}</td>
+                                                <td className="px-3 py-2">
+                                                    <img src={project.image_path} style={{width:70, height:30}} />
+                                                </td>
+                                                <th className="px-3 py-2 hover:underline text-nowrap hover:text-red-100">
+
+                                                    <Link href={route("project.show", project.id)}>
+                                                        {project.name}
+                                                    </Link>
+
+                                                </th>
+                                                <td>
+                                                    <span className={
+                                                        "px-2 py-1 rounded text-white " +
+                                                        PROJECT_STATUS_CLASS_MAP[project.status]
+                                                    }>
+
+                                                    {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                                    </span>
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">{project.created_at}</td>
+                                                <td className="px-3 py-2 text-nowrap">{project.due_date}</td>
+                                                <td className="px-3 py-2">{project.createdBy.name}</td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    <Link href={route('project.edit',project.id)}
+                                                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                    <button
+                                                        onClick={(e) => deleteProject(project)}
+                                                        className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                                    >
+                                                        Delete
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                            </table>
+                        </div>
+                        <Pagination links={projects.meta.links}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </AuthenticatedLayout>
+    )
+}
---
+++ resources/js/Pages/User/Show.jsx
@@ -0,0 +1,123 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link } from "@inertiajs/react";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx"; // mappatura
+import TasksTable from "../Task/TasksTable";
+
+export default function Show({ auth, project, tasks, queryParams }) {
+
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header=
+            {<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {`Project "${project.name}"`}
+            </h2>}
+
+
+        >
+            <Head title={`Project "${project.name}"`} />
+                            {/* <pre  className=" text-wrap h-100">{JSON.stringify(project)}</pre> */}
+        {/* PROJECT */}
+        <div className=" overflow-auto">
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        {/* IMAGE PATH */}
+                        <div>
+                            <img className="w-full h-64 object-cover" src={project.image_path} />
+                        </div>
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <div className="grid gap-1 mt-2 grid-cols-2">
+                                {/* LEFT COLUMNS */}
+                                <div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Project ID</label>
+                                        <div className="mt-1"><p className="flex">N° <strong className="text-green-500 mx-3">{project.id}</strong></p> </div>
+                                    </div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Project Name</label>
+                                        <p className="mt-1">{project.name}</p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Project Status</label>
+                                        <div className="mt-1">
+                                        <span className={
+                                            "px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]
+                                        }>
+                                            {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                        </span>
+                                    </div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Created By</label>
+                                        <p className="mt-1 text-green-500">{project.createdBy.name}</p>
+                                    </div>
+                                </div>
+
+                                </div>
+
+                                {/* RIGHT COLUMNS */}
+                                <div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Due Date</label>
+                                        <p className="mt-1">{project.due_date}</p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Create Date</label>
+                                        <p className="mt-1">{project.created_at}</p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Updated By</label>
+                                        <p className="mt-1">{project.updatedBy.name}</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            {/* SECTION DESCRIPTION AND UPDATE OR DELETE */}
+                            <div>
+                                <div className="mt-4">
+                                        <label className="font-bold text-lg text-nowrap ">Project Description</label>
+                                        <p className="mt-3 font-bold">{project.description}</p>
+                                </div>
+                                <div className="mt-5 mx-3 border flex justify-evenly">
+                                    <Link href={route('project.edit',project.id)}
+                                        className="font-medium  text-yellow-300 dark:text-yellow-300 hover:underline mx-1"
+                                    >
+                                        Edit
+                                    </Link>
+                                    <Link href={route('project.destroy',project.id)}
+                                        className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                    >
+                                        Delete
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        {/* TASKS */}
+        <div className=" overflow-auto">
+            <div className="py-6">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+
+                            <TasksTable tasks={tasks} queryParams={queryParams} hideProjectColumn={true}/>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        
+
+        </AuthenticatedLayout>
+
+    )
+}
